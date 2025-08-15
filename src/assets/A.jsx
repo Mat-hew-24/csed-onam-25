fc96822c3f65c81d125685930f69af74f4bad8c4
Mat-hew-24 <amonline2005@gmail.com>
2025-08-16 01:48:31 +0530

    title working

---
+++ src/assets/A.jsx
@@ -0,0 +1,10 @@
+export default function A() {
+  return (
+    <div>
+      <div className="h-25 w-27 mt-7 ml-12 bg-teal-400 rounded-t-full pl-6 pr-5 pt-6 relative">
+        <div className="h-20 w-15 bg-black  rounded-t-full"></div>
+        <div className="absolute top-12 left-6 w-16 h-6 bg-teal-400"></div>
+      </div>
+    </div>
+  );
+}
---
+++ src/assets/O.jsx
@@ -0,0 +1,9 @@
+export default function O() {
+  return (
+    <div className="w-32 h-32 ml-40">
+      <div className="w-full h-full bg-teal-400 rounded-full flex items-center justify-center">
+        <div className="w-16 h-16 bg-black rounded-full"></div>
+      </div>
+    </div>
+  );
+}
