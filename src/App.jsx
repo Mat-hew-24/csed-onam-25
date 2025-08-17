726055e2cd7233f58f12049e3a7eff58ca29888d
Mat-hew-24 <amonline2005@gmail.com>
2025-08-17 14:50:07 +0530

    animations and content fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Content from "./Content";
+import Header from "./Header";
 
 function App() {
   return (
     <>
       <div className="relative h-[2059px]">
         <div className="box w-screen h-[1536px]">
+          <Header></Header>
           <Content></Content>
         </div>
       </div>
