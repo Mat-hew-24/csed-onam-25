36d3a2b66bd8cdd0eb368ec0244c3c7a719fbbb1
Mat-hew-24 <amonline2005@gmail.com>
2025-08-17 10:39:05 +0530

    content partially arranged

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Content from "./Content";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="relative h-[2059px]">
+        <div className="box w-screen h-[1536px]">
+          <Content></Content>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Content.jsx
@@ -0,0 +1,37 @@
+export default function Content() {
+  return (
+    <>
+      <div className="bg-[#74512D] text-white absolute opacity-100 flex flex-col gap-[41px] pt-[32px] pb-[32px] pl-[65px] pr-[65px] bottom-0 left-0 right-0 h-[1035px]">
+        <div className="para-1 text-[#fffce3]">
+          VADAMVALI • CHENDAMELAM • PAYASAM • DANCE • ONAKKALI
+        </div>
+        <div className="para-2">
+          <h1>CSEA Onam 2025</h1>
+          &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+          <div className="para-2-text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+            voluptatem rem cupiditate labore amet dolorum ullam voluptas soluta
+            nam! Voluptate, magnam error dolores praesentium velit optio iste
+            veritatis laboriosam beatae! Placeat assumenda adipisci amet unde
+            recusandae quisquam, accusantium voluptatem eius, veniam doloremque
+            accusamus sapiente odio sunt quia illo eum. Fugiat in expedita
+          </div>
+        </div>
+        <div className="para-3 mt-10">
+          <h1 className="text-[#fffce3]">Schedule</h1>
+          <div className="para-3-text"></div>
+        </div>
+        <div className="para-4 flex justify-center">
+          vann<span className="onam">onam</span>, thakarth
+          <span className="onam">onam</span>, pwolich
+          <span className="onam">onam</span>
+        </div>
+        <div className="para-5 flex justify-center">
+          <button className="btn-1">instagram</button>
+          <button className="btn-1">youtube</button>
+          <button className="btn-1">minerva</button>
+        </div>
+      </div>
+    </>
+  );
+}
