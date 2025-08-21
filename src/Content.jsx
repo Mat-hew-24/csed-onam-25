cbcb02ee2cd984a9a705c18d204f589775f61e80
Mat-hew-24 <amonline2005@gmail.com>
2025-08-21 13:54:58 +0530

    content added

--- src/Content.jsx
+++ src/Content.jsx
@@ -7,16 +7,76 @@ export default function Content() {
         </div>
         <div className="mb-12 para-2">
           <h1>CSEA Onam 2025</h1>
-          <div className="flex gap-40 flex-wrap-reverse">
-            <div>Lorem ipsum dolor</div>
+          <div className="flex gap-40 scr-wrap-rev">
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+              laudantium libero pariatur quod, fuga rerum ea nemo, facilis
+              labore aliquam eos id magni excepturi culpa optio quisquam eaque
+              non enim? Repellendus, quisquam iste alias quo reiciendis nemo
+              tenetur illo! Obcaecati nobis ab libero in, omnis aliquid,
+              provident suscipit placeat laboriosam sit unde repellat sunt
+              eaque. Ipsam molestiae repellendus quidem nobis. Eius repellat,
+              veritatis non maxime dolore excepturi nam quaerat illo ad
+              temporibus atque dolorem tempora nihil corrupti earum cum? Quidem
+              repudiandae sint at, nemo placeat voluptas ex eum ullam
+              architecto. Facilis itaque error molestiae consectetur, mollitia
+              corporis ducimus voluptatem unde, facere corrupti optio pariatur.
+              Maiores minus voluptates nobis asperiores animi praesentium quo,
+              obcaecati molestiae quidem quae fugiat officia magni repellendus.
+            </div>
             <div>
               <img src="" alt="maveli" />
             </div>
           </div>
         </div>
-        <div className="para-3 mt-10">
+        <div className="para-3 bg-red-600 mt-10">
           <h1 className="text-[#fffce3]">Schedule</h1>
-          <div className="para-3-text"></div>
+          <div className="flex justify-between gap-20 items-center scr-wrap">
+            <div className="flex flex-col">
+              <span>Inauguration Ceremony</span>
+              <div>
+                <span>Lighting of the lamp</span>
+                <span> Traditional invocation song or prayer</span>
+                <span> Welcome speech </span>
+              </div>
+              <div>
+                <span>Cultural Programs</span>
+                <div>
+                  <span>
+                    9:30 AM – 10:30 AM | Thiruvathira Kali (Traditional Dance)
+                  </span>
+                  <span>10:30 AM – 11:00 AM | Onappattu (Onam Songs)</span>
+                  <span>
+                    11:00 AM – 12:00 PM | Skits, Drama, or Mime Stories of
+                    Mahabali & Vamana or humorous short plays
+                  </span>
+                  <span>12:00 PM – 12:30 PM | Games</span>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <span>Inauguration Ceremony</span>
+              <div>
+                <span>Lighting of the lamp</span>
+                <span> Traditional invocation song or prayer</span>
+                <span> Welcome speech </span>
+              </div>
+              <div>
+                <span>Cultural Programs</span>
+                <div>
+                  <span>
+                    9:30 AM – 10:30 AM | Thiruvathira Kali (Traditional Dance)
+                  </span>
+                  <span>10:30 AM – 11:00 AM | Onappattu (Onam Songs)</span>
+                  <span>
+                    11:00 AM – 12:00 PM | Skits, Drama, or Mime Stories of
+                    Mahabali & Vamana or humorous short plays
+                  </span>
+                  <span>12:00 PM – 12:30 PM | Games</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="para-4 flex justify-center">
           <span className="whitespace-nowrap">
