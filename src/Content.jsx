2fb87c4b885af2fab8ff49c5c856886fa83bd580
Mat-hew-24 <amonline2005@gmail.com>
2025-08-23 10:45:33 +0530

    changed to marquee

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,14 +1,23 @@
+import Marquee from "react-fast-marquee";
+
 export default function Content() {
   return (
     <>
       <div>
         <div className="bg-[#74512D] text-white relative opacity-100 flex flex-col gap-[41px] pt-[32px] pb-[32px] pl-[65px] pr-[65px] bottom-0 left-0 right-0 ">
-          <div className="para-1 text-[#fffce3]">
-            VADAMVALI • CHENDAMELAM • PAYASAM • DANCE • ONAKKALI
-          </div>
+          <Marquee
+            gradient={false}
+            speed={150}
+            pauseOnHover
+            className="para-1 text-[#fffce3] text-lg font-bold overflow-hidden [scrollbar-width:none] [&::-webkit-scrollbar]:hidden"
+          >
+            &nbsp;&nbsp;&nbsp;VADAMVALI • CHENDAMELAM • PAYASAM • DANCE •
+            ONAKKALI&nbsp;&nbsp;&nbsp;
+          </Marquee>
+
           <div className="mb-12 para-2">
             <h1>CSEA Onam 2025</h1>
-            <div className="flex gap-40 scr-wrap-rev">
+            <div className="flex gap-40 text-justify scr-wrap-rev">
               <div>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
                 laudantium libero pariatur quod, fuga rerum ea nemo, facilis
@@ -33,44 +42,73 @@ export default function Content() {
           </div>
           <div className="para-3">
             <h1 className="text-[#fffce3]">Schedule</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-12">
+            <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-12">
               <div className="card p-4 border rounded-lg shadow">
-                <span className="schedule-headings">Inauguration Ceremony</span>
-                <ul className="flex flex-col list-disc pl-4">
-                  <li>Lighting of the lamp</li>
-                  <li>Traditional invocation song or prayer</li>
-                  <li>Welcome speech</li>
-                </ul>
+                <div>
+                  <span className="schedule-headings">
+                    Inauguration Ceremony
+                  </span>
+                  <ul className="flex flex-col list-disc pl-4">
+                    <li>Lighting of the lamp</li>
+                    <li>Traditional invocation song or prayer</li>
+                    <li>Welcome speech</li>
+                  </ul>
+                </div>
+                <div className="flex justify-center items-center mt-auto">
+                  <button className="card-btn flex flex-wrap">
+                    <span>Add to Calender</span>
+                  </button>
+                </div>
               </div>
 
               <div className="card p-4 border rounded-lg shadow">
-                <span className="schedule-headings">Cultural Programs</span>
-                <ul className="flex flex-col list-disc pl-4">
-                  <li>9:30 AM – 10:30 AM | Thiruvathira Kali</li>
-                  <li>10:30 AM – 11:00 AM | Onappattu</li>
-                  <li>11:00 AM – 12:00 PM | Skits/Drama</li>
-                  <li>12:00 PM – 12:30 PM | Games</li>
-                </ul>
+                <div>
+                  <span className="schedule-headings">Cultural Programs</span>
+                  <ul className="flex flex-col list-disc pl-4">
+                    <li>9:30 AM – 10:30 AM | Thiruvathira Kali</li>
+                    <li>10:30 AM – 11:00 AM | Onappattu</li>
+                    <li>11:00 AM – 12:00 PM | Skits/Drama</li>
+                    <li>12:00 PM – 12:30 PM | Games</li>
+                  </ul>
+                </div>
+                <div className="flex justify-center items-center mt-auto">
+                  <button className="card-btn flex flex-wrap">
+                    <span>Add to Calender</span>
+                  </button>
+                </div>
               </div>
               <div className="card p-4 border rounded-lg shadow">
-                <span className="schedule-headings">Inauguration Ceremony</span>
-                <ul className="flex flex-col list-disc pl-4">
-                  <li>Lighting of the lamp</li>
-                  <li>Traditional invocation song or prayer</li>
-                  <li>Welcome speech</li>
-                </ul>
+                <div>
+                  <span className="schedule-headings">
+                    Inauguration Ceremony
+                  </span>
+                  <ul className="flex flex-col list-disc pl-4">
+                    <li>Lighting of the lamp</li>
+                    <li>Traditional invocation song or prayer</li>
+                    <li>Welcome speech</li>
+                  </ul>
+                </div>
+                <div className="flex justify-center items-center mt-auto">
+                  <button className="card-btn flex flex-wrap">
+                    <span>Add to Calender</span>
+                  </button>
+                </div>
               </div>
 
               <div className="card p-4 border rounded-lg shadow">
-                <span className="schedule-headings">Cultural Programs</span>
-                <ul className="flex flex-col list-disc pl-4">
-                  <li>9:30 AM – 10:30 AM | Thiruvathira Kali</li>
-                  <li>10:30 AM – 11:00 AM | Onappattu</li>
-                  <li>11:00 AM – 12:00 PM | Skits/Drama</li>
-                  <li>12:00 PM – 12:30 PM | Games</li>
-                </ul>
-                <div className="flex justify-center items-center">
-                  <button className="card-btn">Add to Calender</button>
+                <div>
+                  <span className="schedule-headings">Cultural Programs</span>
+                  <ul className="flex flex-col list-disc pl-4">
+                    <li>9:30 AM – 10:30 AM | Thiruvathira Kali</li>
+                    <li>10:30 AM – 11:00 AM | Onappattu</li>
+                    <li>11:00 AM – 12:00 PM | Skits/Drama</li>
+                    <li>12:00 PM – 12:30 PM | Games</li>
+                  </ul>
+                </div>
+                <div className="flex justify-center items-center mt-auto">
+                  <button className="card-btn flex flex-wrap">
+                    <span>Add to Calender</span>
+                  </button>
                 </div>
               </div>
             </div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,31 +1,33 @@
+import Marquee from "react-fast-marquee";
+
 export default function Header() {
   return (
-    <div className="headerBox">
-      <div className="scrollContent">
-        <div className="flex justify-center items-center ">
+    <>
+      <Marquee speed={150} gradient={false}>
+        <div className="flex items-center gap-6">
           <img
             src="b94063ae9050f2fe1a30a9a4912c15fcbea3d5a0 (1).png"
             alt="sponsor"
-            width={186}
-            height={75}
+            className="h-[75px] w-[186px] object-contain flex-shrink-0"
           />
 
-          <div className="headerBullet">•</div>
+          <div className="headerBullet text-2xl">•</div>
+
           <img
             src="7be119cfb655f9cfc07ce60b492da2e91012c16d (1).png"
             alt="sponsor"
-            width={227}
-            height={75}
+            className="h-[75px] w-[227px] object-contain flex-shrink-0"
           />
-          <div className="headerBullet">•</div>
+
+          <div className="headerBullet text-2xl">•</div>
+
           <img
             src="5481f1ced918f56d48c3a0c68c75c5dc6579fe12.png"
             alt="sponsor"
-            width={121}
-            height={75}
+            className="h-[75px] w-[121px] object-contain flex-shrink-0"
           />
         </div>
-      </div>
-    </div>
+      </Marquee>
+    </>
   );
 }
